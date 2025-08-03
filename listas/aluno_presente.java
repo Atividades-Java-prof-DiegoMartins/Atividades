5feaf051996931690c0e25465c2d6fa76379259b
Jeniffer Dinz <73167898+jenifferdnz@users.noreply.github.com>
2025-08-03 18:18:23 -0300

    correção de atividade

---
+++ listas/aluno_presente.java
@@ -0,0 +1,30 @@
+package listas;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner; 
+public class aluno_presente {
+        public static void main(String[] args) {
+            ArrayList<String> alunos = new ArrayList<>(Arrays.asList(
+                "Ana", "Bruno", "Carlos", "Daniela", "Eduardo"
+            ));
+
+            Scanner sc = new Scanner(System.in);
+
+            System.out.println("Informe o seu nome: ");
+            String nome = sc.nextLine();
+
+            for (String nomealuno:alunos){
+
+                if (nome.equals(nomealuno) ){
+                    System.out.println("PRESENTE");
+                } 
+                
+            }
+
+
+
+        }
+
+
+    
+}
--- listas/atividade_numeros_pares.java
+++ listas/atividade_numeros_pares.java
@@ -7,27 +7,20 @@ public class atividade_numeros_pares {
         ArrayList<Integer>listanum=new ArrayList();
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("Me informe um número: ");
-        int numum = sc.nextInt();
-        System.out.println("Informe o segundo número: ");
-        int numsegund = sc.nextInt();
-        System.out.println("Informe o terceiro número: ");
-        int numterc = sc.nextInt();
-        System.out.println("Informe o quarto número: ");
-        int numquarto = sc.nextInt();
-        System.out.println("Informe o quinto número: ");
-        int numquinto = sc.nextInt();
+        System.out.println("Me informe cinco números: ");
+        //int numumeros = sc.nextInt();
+        //System.out.println("Informe o segundo número: ");
+        //int numsegund = sc.nextInt();
+        //DÚVIDA: Eu gostaria de pedir 5x que o usuário adicione um número.
+        
 
-        listanum.add(numum);
-        listanum.add(numsegund);
-        listanum.add(numterc);
-        listanum.add(numquarto);
-        listanum.add(numquinto);
-
-        //System.out.println(listanum.get(0));
-        //System.out.println(listanum.get(1));
-
-
+        listanum.add(sc.nextInt());
+        listanum.add(sc.nextInt());
+        listanum.add(sc.nextInt());
+        listanum.add(sc.nextInt());
+        listanum.add(sc.nextInt());
+        //System.out.println(listanum + "é a lista");
+        
         for (Integer numeross:listanum){
             if(numeross % 2 == 0){ //descobri que, ao colocar a variável numeross, a lista será varrida e vai exibir no print apenas números pares.
                 System.out.println("Números PARES digitados " + numeross);
@@ -37,28 +30,4 @@ public class atividade_numeros_pares {
             
     }
 
-    
 }
-
-            //TENTATIVAS DE CÓDIGO
-            //else{
-                //System.out.println(numeross + " Não é PAR!");
-            //}
-            
-            //if (numsegund % 2 == 0){ //Não consegui colocar um else if aqui, por que?
-                //System.out.println(numsegund + "é PAR");
-            //}else {
-                //System.out.println(numsegund + "NÃO é PAR");
-            //}
-            
-            //System.out.println(numeross);
-            //if(numum % 2 == 0){
-                //System.out.println(numum + "é PAR!");
-
-            //}else if(numum != 0){
-                //System.out.println(numum + "NÃO é PAR");
-            //}if (numsegund % 2 == 0){ //Não consegui colocar um else if aqui, por que?
-                //System.out.println(numsegund + "é PAR");
-            //}else {
-                //System.out.println(numsegund + "NÃO é PAR");
-            //}
