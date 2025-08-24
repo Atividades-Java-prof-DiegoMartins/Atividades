4e4a677e83dc695ae6d0360d0d7e27cece888fa5
Jeniffer Dinz <73167898+jenifferdnz@users.noreply.github.com>
2025-08-23 21:09:41 -0300

    Atividade contagem regressiva

---
+++ estrutura_repetição/contagem_regressiva.java
@@ -0,0 +1,23 @@
+package estrutura_repetição;
+import java.util.Scanner;
+
+public class contagem_regressiva {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i = 6;
+        //for (i=5; i>0; i--){
+            //System.out.println(i);
+
+        //}
+
+        while(i < 7 && i >0){
+            i = i-1;
+            System.out.println(i);
+
+        }
+        
+        
+    }
+
+    
+}
