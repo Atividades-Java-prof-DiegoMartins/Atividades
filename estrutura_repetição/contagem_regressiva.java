f6971dcae96732c3cf17ce5fc91c0b0d8d92f7d9
Jeniffer Dinz <73167898+jenifferdnz@users.noreply.github.com>
2025-09-01 22:20:52 -0300

    atividade menu realizada

--- estrutura_repetição/contagem_regressiva.java
+++ estrutura_repetição/contagem_regressiva.java
@@ -5,10 +5,6 @@ public class contagem_regressiva {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        //for (i=5; i>0; i--){
-            //System.out.println(i);
-
-        //}
         System.out.println("Inicie a contagem regressiva: ");
         int i = sc.nextInt();
         while(i>0){
---
+++ estrutura_repetição/menu.java
@@ -0,0 +1,39 @@
+package estrutura_repetição;
+import java.util.Scanner;
+
+
+public class menu {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //int opcao = sc.nextInt();
+
+        System.out.println("Bem-vindo(a) ao restaurante!");
+        System.out.println("============================");
+
+        System.out.println("Selecione 1 para doces, 2 para salgados e 0 para sair: ");
+        int opcao = sc.nextInt();
+
+        while(opcao ==1 || opcao ==2){
+            if (opcao ==1){
+                System.out.println("Doces");
+                System.out.println("- Doce 1 (R$ 10,90)");
+                System.out.println("- Doce 2 (R$ 12,90)");
+                System.out.println("- Doce 3 (R$ 15,60)");
+                System.out.println("Selecione 1 para doces, 2 para salgados e 0 para sair: ");
+                opcao = sc.nextInt();
+            }else if (opcao == 2){
+                System.out.println("Salgados");
+                System.out.println("- Salgado 1 (R$ 10,90)");
+                System.out.println("- Salgado 2 (R$ 12,90)");
+                System.out.println("- Salgado 3 (R$ 15,60)");
+                System.out.println("Selecione 1 para doces, 2 para salgados e 0 para sair: ");
+                opcao = sc.nextInt();
+            } else if (opcao == 0) {
+                sc.close();
+            }
+        }
+        
+
+    }
+    
+}
