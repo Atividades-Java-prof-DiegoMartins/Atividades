32b0c43e4f37fe1fd7eb00ed3cf296cec7d0c62a
Jeniffer Dinz <73167898+jenifferdnz@users.noreply.github.com>
2025-09-08 21:08:09 -0300

    Update lojinha.java
    Atividade em andamento.

--- projetos/lojinha.java
+++ projetos/lojinha.java
@@ -34,6 +34,23 @@ public class lojinha {
                 System.out.println("produtos cadastrados " + cadastrados);
 
             }else if(opcao == 3){
+                for (String produtlis:cadastrados){
+                    System.out.println(cadastrados);
+                    System.out.println("Informe qual produto deseja alterar: ");
+                    String produtlist = sc.nextLine();
+                    
+                    if (cadastrados.contains(produtlist)){
+                        System.out.println("digite a alteração");
+                        String produtnew = sc.nextLine();
+                        cadastrados.remove(produtlist);
+                        cadastrados.add(produtnew);
+                        System.out.println("Nova lista " + cadastrados);
+
+
+                    }
+                }
+
+
                 System.out.println("Atualizar produtos");
             }else if(opcao == 4){
                 System.out.println("Remover produto");
